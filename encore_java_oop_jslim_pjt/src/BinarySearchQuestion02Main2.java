83a449360e83fbbcf87ee204a92009291abafc4a
jslim <jslim9413@naver.com>
2024-02-22 16:18:54 +0900

    17일차 commit

--- encore_java_oop_jslim_pjt/src/BinarySearchQuestion02Main2.java
+++ encore_java_oop_jslim_pjt/src/BinarySearchQuestion02Main2.java
@@ -13,6 +13,7 @@
 15
 */
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class BinarySearchQuestion02Main2 {
@@ -35,6 +36,31 @@ public class BinarySearchQuestion02Main2 {
 	public static int search(Integer [] ary , int target) {
 		int result = -1 ;
 		
+		Arrays.sort(ary , Collections.reverseOrder() );
+		int low  = 0 ; 
+		int high = ary[0] ;  
+		System.out.println("low " + low + " high " + high) ;  
+		while(low <= high) {
+			int mid = (low + high) / 2 ; 
+			System.out.println("mid >>>>> " + mid) ; 
+			
+			int cnt = 0 ; 
+			for(int cake : ary) {
+				if( cake > mid ) {
+					cnt += ( cake - mid ) ;
+					System.out.println("cake  " + cake + "\t mid " + mid);
+				}
+			}
+			System.out.println("cnt : " + cnt );
+			if( target == cnt ) {
+				result = mid ; 
+				break ; 
+			} else if( target < cnt ) {
+				low = mid + 1 ; 
+			} else {
+				high = mid - 1 ; 
+			}
+		}
 		return result;  
 	}
 
---
+++ encore_java_oop_jslim_pjt/src/DfsAlgorithmMain.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DfsAlgorithmMain {
+
+	public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>(); 
+	public static boolean [] visited = new boolean[9] ; 
+	
+	public static void main(String[] args) {
+		// 요소를 담을 수 있는 리스트 생성(사이즈 만큼) ; 
+		for(int i = 0 ; i < visited.length ; i++) {
+			graph.add( new ArrayList<Integer>() );
+		}
+		
+		// 1 번 노드에 연결된 노드 정보.
+		graph.get(1).add(2); graph.get(1).add(3); graph.get(1).add(8);
+		graph.get(2).add(1); graph.get(2).add(7); 
+		graph.get(3).add(1); graph.get(3).add(4); graph.get(3).add(5);
+        graph.get(4).add(3); graph.get(4).add(5);
+        graph.get(5).add(3); graph.get(5).add(4);
+        graph.get(6).add(7);
+        graph.get(7).add(2); graph.get(7).add(6); graph.get(7).add(8);
+        graph.get(8).add(1); graph.get(8).add(7); 
+        
+        dfs(1);
+        System.out.println();
+        for( boolean flag : visited ) {
+			System.out.print(flag + "\t");
+		}
+	}
+	
+	public static void dfs(int start) {
+		System.out.print(start + "\t"); 
+		visited[start] = true ; 
+		for( int node : graph.get(start) ) {
+			if( !visited[node] ) {
+				dfs(node) ;
+			}
+		}
+		
+	}
+	
+
+}
+
+
+
+
+
